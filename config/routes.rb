2bfd4503dd5085a4489ccc6a5947714977b64493
MustafaaOzdemir <mostafaatya077@gmail.com>
2025-08-27 23:01:57 +0300

    rails book-store CRUD

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  resources :books
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "/books", to: "books#index"
+  get "/books/:id", to: "books#show"
+  get "/books/new", to: "books#new"
+  post "/books", to: "books#create"
+  get "/books/:id/edit", to: "books#edit"
+  put "/books/:id", to: "books#update"
+  delete "/books/:id", to: "books#destroy"
+  # post "/books", to: "books#create"
+  # get "/books/:id/edit", to: "books#edit"
+  # get "/books/:id", to: "books#show"
+  # put "/books/:id", to: "books#update"
+  # delete "/books/:id", to: "books#destroy"
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
